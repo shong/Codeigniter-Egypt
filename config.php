83ab39150f88fc0062c7e155303cf1ddbe223e17
shong <sewoom@gmail.com>
2011-09-12 09:55:36 -0700

    Edited config.php via GitHub

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config_site_name		= "Site Name";
 
-$db_hostname			= "localhost";
+$db_hostname			= "10.194.111.8";
 $db_database			= "egypt";
 $db_username			= "root";
 $db_password			= "1234";
