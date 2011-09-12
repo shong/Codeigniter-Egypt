dcc90b38f29f82510c8b5e6497b807e66f9c1033
shong <sewoom@gmail.com>
2011-09-12 10:05:51 -0700

    Edited config.php via GitHub

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ $config_site_name		= "Site Name";
 $db_hostname			= "10.194.111.8";
 $db_database			= "db_a759ad4a";
 $db_username			= "user_a759ad4a";
-$db_password			= "1234";
-$db_dbdriver			= "cq9yb;GONvEt3x";
+$db_password			= "cq9yb;GONvEt3x";
+$db_dbdriver			= "mysql";
